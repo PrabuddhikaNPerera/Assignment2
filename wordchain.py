3f267a2fff0e180af7bf4337a497efdae86d8743
prabu <prabuddhike@gmail.com>
2024-09-26 13:02:13 +0800

    Initial Commit

--- wordchain.py
+++ wordchain.py
@@ -1,6 +1,18 @@
 #Student Name : Prabuddhika Panawalage
 #Student ID : 10609924
 
+def inputNumber(prompt):
+    while True:
+        try:
+            number = int(input(prompt))
+            if number < 2:
+                print("Error: The number must be greater than of equal 2.") # Check if the number is greater than or equal 2
+            else:
+                return number
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
+
 def main():
     #Define variables
     numberofplayers = 0
@@ -8,6 +20,10 @@ def main():
 
     #Welcome message
     print("Welcome to WordChain!")
+
+    # Get number of players
+    numberofplayers = inputNumber("Enter the number of players (minimum 2): ")
+    print (numberofplayers)
 
 
 
